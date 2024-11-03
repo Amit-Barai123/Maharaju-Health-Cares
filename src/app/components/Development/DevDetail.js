b103044c2e13d6ce2aedc0ea5257f9236a041b65
Amit Barai <155969547+Amit-Barai123@users.noreply.github.com>
2024-11-03 03:29:40 -0800

    Update DevDetail.js

--- src/app/components/Development/DevDetail.js
+++ src/app/components/Development/DevDetail.js
@@ -4,7 +4,7 @@ export default function DevDetails() {
         {/* Left Side - Image */}
         <div className="md:w-1/2 w-full h-full flex items-center justify-center slide-left">
           <img
-            src="/images/dev-det-1.jpg" // Replace with your image path
+            src="/images/dev-det-1.JPG" // Replace with your image path
             alt="Team Meeting"
             className="object-cover w-[80%] h-auto"
           />
